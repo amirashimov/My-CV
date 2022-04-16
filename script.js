a858f29c3783e06a81a27c2d5e05a147373f8d61
Amirkhan <101048748+amirashimov@users.noreply.github.com>
2022-04-16 16:09:14 +0600

    Create CV

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let number = 35;
+
+// const result = confirm("Are you here?");
+//const answer = prompt("Are you 18?", "");
+//console.log(answer);
+const answers = [];
+answers[0] = prompt("What is your name", "");
+answers[1] = prompt("What is your lastname","");
+answers[2] = prompt("How old are you?", "");
+document.write(answers);
